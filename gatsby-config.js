c943cd290645ebff1141cf02d6284ade8cad3149
unknown <luis.ramirez72762@gmail.com>
2023-07-28 10:29:01 -0500

    Added PokeAPI plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,13 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-pokeapi`,
+      options: {
+        nbOfPokemons: 100000,
+      },
+    },
+  ],
 };
