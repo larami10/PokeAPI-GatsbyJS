8834c768bc5e0900ace2e30f232307fa34705556
unknown <luis.ramirez72762@gmail.com>
2023-08-02 10:10:27 -0500

    Remove scroll to top for better user experience

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import "./src/styles/global.scss";
 
 // return false to start each page at the top of the page
-export const shouldUpdateScroll = () => {
-  return false;
-};
+// export const shouldUpdateScroll = () => {
+//   return false;
+// };
